bda526dbb037b4ba5d7c7c75df0efd9f12492cc1
Made-of-Clay <adam.m.leis@gmail.com>
2016-06-22 10:16:52 -0400

    add pluginClass to elems on init; add some util funcs

--- dist/js/jquery.bindtabs.js
+++ dist/js/jquery.bindtabs.js
@@ -1,4 +1,6 @@
 /// <reference path="./jquery.d.ts" />
+/// <reference path="./bindtabs-options.d.ts" />
+;
 (function pluginSetupWrapper() {
     var pluginName = 'bindTabs', pluginNs = 'moc';
     var defaults = {
@@ -6,28 +8,82 @@
     };
     var BindTabs = (function () {
         function BindTabs(element, options) {
+            this._pluginClass = 'bind_tabs';
             this.element = $(element);
             this.options = $.extend({}, defaults, options);
             this._name = pluginName;
             this._defaults = defaults;
             this._create();
+            return this;
         }
         BindTabs.prototype._create = function () {
-            this.element.addClass('vehicle-class');
-            _initListeners(this);
+            this._checkOptions();
+            this._addPluginMarkup();
+            this._initListeners();
+        };
+        BindTabs.prototype._checkOptions = function () {
+            var opts = this.options;
+            // assign tabs & containers
+            if (!isEmpty(opts.tabs) && opts.containers) {
+                this._assignRelations('tabs', opts.tabs);
+                this._assignRelations('containers', opts.containers);
+            }
+        };
+        BindTabs.prototype._assignRelations = function (prop, elem) {
+            var plugin = this;
+            if (isJQuery(elem) && elem.length > 0) {
+                plugin[prop] = elem;
+            }
+            else {
+                var findTabs = plugin.element.find(elem).not("." + plugin._pluginClass);
+                if (findTabs.length > 0) {
+                    plugin[prop] = findTabs;
+                }
+            }
+        };
+        BindTabs.prototype._addPluginMarkup = function () {
+            this._addPluginClass();
+        };
+        BindTabs.prototype._addPluginClass = function () {
+            var plugin = this;
+            $.each([plugin.element, plugin.tabs, plugin.containers], function (index, item) {
+                $(item).addClass(plugin._pluginClass);
+            });
+        };
+        BindTabs.prototype._initListeners = function () {
+            var plugin = this;
+            plugin.element;
         };
         return BindTabs;
     }());
-    function _initListeners(plugin) {
-        plugin.element.on('click', 'button.horn', function (event) {
-            plugin.honk();
-        });
+    // Utilities
+    function isJQuery(toCheck) {
+        return toCheck instanceof jQuery;
+    }
+    function isSet(toCheck) {
+        return typeof toCheck !== 'undefined';
+    }
+    function isEmpty(mixed_var) {
+        var undef, key, i, len;
+        var emptyValues = [undef, null, false, 0, '', '0'];
+        for (i = 0, len = emptyValues.length; i < len; i++) {
+            if (mixed_var === emptyValues[i]) {
+                return true;
+            }
+        }
+        if (typeof mixed_var === 'object') {
+            for (key in mixed_var) {
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
     $.fn[pluginName] = function (options) {
-        var namespaced = pluginName + "-" + pluginNs;
+        var namespaced = pluginNs + "-" + pluginName;
         return this.each(function () {
             if (!$(this).data(namespaced)) {
-                $(this).data(namespaced), new BindTabs(this, options);
+                $(this).data(namespaced, new BindTabs(this, options));
             }
         });
     };
--- src/scripts/jquery.bindtabs.ts
+++ src/scripts/jquery.bindtabs.ts
@@ -1,5 +1,6 @@
 /// <reference path="./jquery.d.ts" />
-(function pluginSetupWrapper() {
+/// <reference path="./bindtabs-options.d.ts" />
+;(function pluginSetupWrapper() {
     var pluginName: string = 'bindTabs',
         pluginNs: string = 'moc';
     var defaults = {
@@ -8,38 +9,101 @@
 
     class BindTabs {
         element: JQuery;
-        options: Object;
+        tabs: JQuery;
+        containers: JQuery;
+
+        options: BtOptions;
+
         _name: String;
         _defaults: Object;
 
+        _pluginClass = 'bind_tabs';
+
         constructor(element, options) {
             this.element = $(element);
-            this.options = $.extend({}, defaults, options);
+            this.options = $.extend({}, defaults, options) as BtOptions;
             this._name = pluginName;
             this._defaults = defaults;
             this._create();
+            return this;
         }
 
         _create() {
-            this.element.addClass('vehicle-class');
-            _initListeners(this);
+            this._checkOptions();
+            this._addPluginMarkup();
+            this._initListeners();
         }
 
-    //     honk() {
-    //         alert('beep beep');
-    //     }
+        _checkOptions() {
+            var opts = this.options;
+            // assign tabs & containers
+            if(!isEmpty(opts.tabs) && opts.containers) {
+                this._assignRelations('tabs', opts.tabs);
+                this._assignRelations('containers', opts.containers);
+            }
+        }
+        _assignRelations(prop, elem) {
+            var plugin = this;
+
+            if(isJQuery(elem) && elem.length > 0) {
+                plugin[prop] = elem;
+            } else {
+                var findTabs = plugin.element.find(elem).not(`.${plugin._pluginClass}`);
+                if(findTabs.length > 0) {
+                    plugin[prop] = findTabs;
+                }
+            }
+        }
+
+        _addPluginMarkup() {
+            this._addPluginClass();
+        }
+        _addPluginClass() {
+            var plugin = this;
+            $.each([plugin.element, plugin.tabs, plugin.containers], (index, item) => {
+                $(item).addClass(plugin._pluginClass);
+            });
+        }
+        
+        _initListeners() {
+            var plugin = this;
+            plugin.element
+            ;
+        }
     }
-    function _initListeners(plugin) {
-        plugin.element.on('click', 'button.horn', (event) => {
-            plugin.honk();
-        });
+
+    // Utilities
+    function isJQuery(toCheck) {
+        return toCheck instanceof jQuery;
+    }
+    function isSet(toCheck) {
+        return typeof toCheck !== 'undefined';
+    }
+    function isEmpty(mixed_var) {
+        var undef, key, i, len;
+        var emptyValues = [undef, null, false, 0, '', '0'];
+
+        for (i = 0, len = emptyValues.length; i < len; i++) {
+            if (mixed_var === emptyValues[i]) {
+                return true;
+            }
+        }
+
+        if (typeof mixed_var === 'object') {
+            for (key in mixed_var) {
+                return false;
+            }
+            return true;
+        }
+
+        return false;
     }
 
     $.fn[pluginName] = function(options: Object) {
-        var namespaced: string = `${pluginName}-${pluginNs}`;
+        var namespaced: string = `${pluginNs}-${pluginName}`;
         return this.each(function() {
             if (!$(this).data(namespaced)) {
-                $(this).data(namespaced), new BindTabs(this, options);
+                $(this).data(namespaced, new BindTabs(this, options));
             }
         });
     }
