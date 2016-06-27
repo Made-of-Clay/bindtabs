75c60ca390ce219ad7aaa1b6c25bc1b861b1e830
Made-of-Clay <adam.m.leis@gmail.com>
2016-06-27 15:39:19 -0400

    add test styles; add closable option to interface

--- src/scripts/bindtabs-options.d.ts
+++ src/scripts/bindtabs-options.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="./jquery.d.ts" />
 interface BtOptions {
     tabs: HTMLElement | String | JQuery,
-    containers: HTMLElement | String | JQuery
+    containers: HTMLElement | String | JQuery,
+    closable: boolean
 }
